33e240dda3a2c158a1d4345c436142181b3443fd
Venom <78640257+Venom9718@users.noreply.github.com>
2021-08-03 09:47:32 +0530

    Update client.js

--- handlers/client.js
+++ handlers/client.js
@@ -9,4 +9,6 @@ module.exports = async (client) => {
     client.commands = new Collection();
     client.slashcommands = new Collection();
     client.config = require("../config.json");
+    
+    if(!client.config.token || !client.config.prefix) throw new Error("Please provide the required things in config.json")
 }
